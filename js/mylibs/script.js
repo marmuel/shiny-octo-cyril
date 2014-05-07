2dd29f734a50d089ccee4247dac08c95e5a30dca
marmuel <marco.mueller@haufe-lexware.net>
2014-05-07 19:10:33 +0200

    issues in translation.json

---
+++ js/mylibs/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() { 
+	
+// configuration table x-editable
+
+
+	
+ });
+
+$(function(){
+    $('#document-table a').editable({
+        //url: '/post',
+        title: 'Enter Data',
+        rows: 1
+    });
+});
+
+$.fn.editable.defaults.mode = 'inline';	
+
+
+
+
+
