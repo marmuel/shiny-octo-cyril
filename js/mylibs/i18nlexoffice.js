c430d2136f628b54b6bd190f84b63770d54730b3
marmuel <mmoogles@googlemail.com>
2014-05-11 08:52:29 +0200

    ...

--- js/mylibs/i18nlexoffice.js
+++ js/mylibs/i18nlexoffice.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
+i18n.init({ fallbackLng: 'en-US'
+ });
+
 i18n.init(function(t) {
   // translate nav
   $(".nav").i18n();
   $(".document-wrapper").i18n();
-  
-  // programatical access
-  var appName = t("app.i18nlexoffice");
+
 });
 
 });
