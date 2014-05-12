1cc971a4debb75e46f97ea84f6e6f767aaf0e843
marmuel <mmoogles@googlemail.com>
2014-05-12 17:57:53 +0200

    - add tax row dynamically - add colspan to tfoot dynamical depending on tax-columns

--- js/mylibs/script.js
+++ js/mylibs/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-	   // trigger default values
+	// empty all inputs of tbody
+
+	$("#document-table> tbody").not(':first').empty();
+
+	// trigger default values
 	$('#shippingno').trigger('click');
 	$('#searchno').trigger('click');
+	$('.tax-subtotal').hide();
 	$("#tax").trigger("change");
 	$("#currency").trigger("change");
-	
+	$("#discount").trigger("change");
+
 	// add class for styling
 	$("#document-table").tableDnD({
 		onDragClass : "myDragClass"
@@ -19,8 +25,6 @@ $(document).ready(function() {
 
 	// set all textareas to autosize
 	$('textarea').autosize();
-	// set defaults discount, shipping and tax
-	SetDiscount();
 
 	// destroy autosize for different inputs
 	$('#document-tocompany').trigger('autosize.destroy');
@@ -35,8 +39,8 @@ $("#button-rowadd").click(function() {
 	var row = $("#document-table tbody>tr:last").clone(true);
 	$("td input:text", row).val(0);
 	$(".quantity", row).val(1);
-	$(".tax1-row", row).val('0%');
-	$(".tax2-row", row).val('0%');
+	$(".tax1-row", row).val('');
+	$(".tax2-row", row).val('');
 
 	$(row.insertAfter('#document-table tbody>tr:last')).fadeIn();
 
@@ -81,29 +85,22 @@ $("#document-table").on('click', ".remove-row", function() {
 });
 
 // Discount or not?
-function SetDiscount() {
-	var dissettings = $("#discount option:selected").index();
+$("#discount").change(function() {
+	var dissettings = $("#discount").val();
 	var drow = $(".discount-row").parents('tr');
-	if (dissettings == 0) {
+	if (dissettings == "") {
 		// no discount
 		$(drow).fadeOut();
 		$(".discount-total").val(0);
 	}
-	if (dissettings == 1) {
-		// discount flat
+	if (dissettings != "") {
+		// discount
 		$(drow).fadeIn();
 		$(drow).i18n();
 		// add currency to labels
 		$("#currency").trigger("change");
 	}
-	if (dissettings == 2) {
-		// discount percent
-		$(drow).fadeIn();
-		$(drow).i18n();
-		// add currency to labels
-		$("#currency").trigger("change");
-	}
-}
+});
 
 // Shipping or not?
 var $delbtnyes = $('#shippingyes');
@@ -119,58 +116,56 @@ $delbtnno.on('click', function() {
 	$(srow).fadeOut();
 	$(".shipping-total").val(0);
 });
+var $taxes = $('.taxes');
+$taxes.on('change', function() {
 
+   $('.tax-subtotal').show();
+   return false;
+   
+	$($taxes).each(function() {
+		if ($(this).val().length == 0) {
+			return;
+		};
+		if ($(this).val().length !== 0) {
+			// add new taxrow
+			$trLast = $('#document-table').find("tr.taxrow:last");
+
+			$trNew = $trLast.clone();
+			$trLast.after($trNew);
+			$("#currency").trigger("change");
+
+		};
+	});
+});
+
+// add dynamically rows for taxes 
 $('#tax').change(function() {
 	var taxessettings = $("#tax option:selected").index();
-	var trow1 = $(".tax-row1").parents('tr');
-	var trow2 = $(".tax-row2").parents('tr');
-	if (taxessettings == 0) {
-		// no taxes
-		$(trow1).fadeOut();
-		$(trow2).fadeOut();
-		$(".tax-total1").val(0);
-		$(".tax-total2").val(0);
-	}
-	
-	if (taxessettings ==1) {
-		// tax 1
-		$(trow1).fadeIn();
-		$(trow2).fadeOut();
-		$(".tax-total2").val(0);
-	}
-
-if (taxessettings ==2) {
-		// tax 1
-		$(trow1).fadeIn();
-		$(trow2).fadeIn();
-	}
- 
-	var $tax1 = $('#document-table').find('tbody tr').find('td:nth-child(5)'), $tax1th = $('#document-table').find('thead th:nth-child(5)'), $tax1ft = $('#document-table').find('tfoot tr').find('td:nth-child(3)'), $tax2 = $('#document-table').find('tbody tr').find('td:nth-child(6)'), $tax2th = $('#document-table').find('thead th:nth-child(6)'), $tax2ft = $('#document-table').find('tfoot tr').find('td:nth-child(4)'), $v = $(this).val();
+	var $tax1 = $('#document-table').find('tbody tr').find('td:nth-child(5)'), $tax1th = $('#document-table').find('thead th:nth-child(5)'),
+	$tax2 = $('#document-table').find('tbody tr').find('td:nth-child(6)'), $tax2th = $('#document-table').find('thead th:nth-child(6)'),
+	$v = $(this).val();
 
 	if ($v == 'tax-none') {
 		$tax1.fadeOut();
 		$tax1th.fadeOut();
-		$tax1ft.fadeOut();
 		$tax2.fadeOut();
 		$tax2th.fadeOut();
-		$tax2ft.fadeOut();
+		$('.footer-labels').attr('colspan',1);
 	}
 
 	if ($v == '1 Tax') {
 		$tax1.fadeIn();
 		$tax1th.fadeIn();
-		$tax1ft.fadeIn();
 		$tax2.fadeOut();
 		$tax2th.fadeOut();
-		$tax2ft.fadeOut();
+		$('td.footer-labels').attr('colspan',2);
 	}
 	if ($v == '2 Taxes') {
 		$tax1.fadeIn();
 		$tax1th.fadeIn();
-		$tax1ft.fadeIn();
 		$tax2.fadeIn();
 		$tax2th.fadeIn();
-		$tax2ft.fadeIn();
+		$('td.footer-labels').attr('colspan',3);
 	}
 
 });
@@ -191,14 +186,9 @@ $('#currency').change(function() {
 
 	// add currency to labels
 
-	var dissettings = $("#discount option:selected").index();
 	var cur = $('#currency').val();
-	var percent = "%";
 	$('.currency-label').val(cur);
 	$('.discount-row-label').val(cur);
-	if (dissettings == 2) {
-		$('.discount-row-label').val(percent);
-	}
 });
 
 // autocomplete feature
@@ -224,7 +214,7 @@ $gbtnyes.on('click', function() {
 $gbtnno.on('click', function() {
 	// no google address search
 	(manaddress).show();
-	(locationfield).hide();	
+	(locationfield).hide();
 	(gaddressResult).hide();
 });
 
@@ -313,4 +303,6 @@ function geolocate() {
 		});
 	}
 }
+
+
 
